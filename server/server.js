bdd4cc1c340aa135346268f5330a7627e47f1d3b
Mohamed Khaled <mo.khaled3003@gmail.com>
2019-07-27 23:56:22 +0200

    ready to deploy

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 
 var {mongoose} = require('./db/mongoose');
@@ -24,6 +25,20 @@ app.get('/todos',(req,res)=>{
         res.send({todos});
     },(er)=>{
         res.status(400).send(er);
+    });
+});
+app.get('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo)=>{
+        if(!todo){
+           return res.status(404).send();
+        }
+        res.send(JSON.stringify(todo,undefined,2));
+    }).catch((err)=>{
+         res.status(400).send();
     });
 });
 
