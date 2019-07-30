f7592b6af335dcbfda6e649654000d1f6e191585
Mohamed Khaled <mo.khaled3003@gmail.com>
2019-07-30 06:29:52 +0200

    adding JWT to users

--- server/server.js
+++ server/server.js
@@ -79,6 +79,19 @@ app.patch('/todos/:id',(req,res)=>{
         return res.status(400).send();
     });
 });
+app.post('/users',(req,res)=>{
+    var body = _.pick(req.body,["email","password"]);
+    var user = new User(body);
+    user.save().then((user)=>{
+       return user.generateAuthToken();
+    }).then((token)=>{
+        res.header('x-auth',token).send(user);
+    })
+    .catch((er)=>{
+        res.status(400).send(er);
+    });
+
+});
 
 app.listen(port,()=>{
     console.log(`the server is on at port ${port}`);
